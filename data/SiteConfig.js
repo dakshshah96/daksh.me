03cd60c35e4cd7d50581e252553329da047362e7
Daksh Shah <dakshshah96@gmail.com>
2020-04-09 14:05:05 +0530

    update site config fields

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,26 +1,22 @@
 const config = {
-  siteTitle: "Gatsby Advanced Starter", // Site title.
-  siteTitleShort: "GA Starter", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "GatsbyJS Advanced Starter", // Alternative site title for SEO.
-  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://gatsby-advanced-starter-demo.netlify.com", // Domain of your website without pathPrefix.
-  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "A GatsbyJS stater with Advanced design in mind.", // Website description used for RSS feeds/meta description tag.
-  siteRss: "/rss.xml", // Path to the RSS file.
-  siteRssTitle: "Gatsby Advanced Starter RSS feed", // Title of the RSS feed
-  googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
-  dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
-  dateFormat: "DD/MM/YYYY", // Date format for display.
-  postsPerPage: 4, // Amount of posts displayed per listing page.
-  userName: "Advanced User", // Username to display in the author segment.
-  userEmail: "AdvancedUser@example.com", // Email used for RSS feed's author segment
-  userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "North Pole, Earth", // User location to display in the author segment.
-  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
-  userDescription:
-    "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
-  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
-  backgroundColor: "#e0e0e0" // Used for setting manifest background color.
+  siteTitle: "Daksh Shah",
+  siteTitleShort: "Daksh Shah",
+  siteTitleAlt: "Daksh Shah",
+  siteLogo: "/logos/logo-1024.png",
+  siteUrl: "https://daksh.me",
+  repo: "https://github.com/dakshshah96/daksh.me",
+  pathPrefix: "",
+  siteDescription: "Daksh Shah is a full stack software developer.",
+  siteRss: "/rss.xml",
+  googleAnalyticsID: "UA-47311644-5",
+  dateFromFormat: "YYYY-MM-DD",
+  dateFormat: "MMMM Do, YYYY",
+  postsPerPage: 4,
+  userName: "Daksh",
+  userEmail: "dakshshah96@gmail.com",
+  userTwitter: "dakshshah96",
+  themeColor: "#c62828",
+  backgroundColor: "#e0e0e0"
 };
 
 // Validate
