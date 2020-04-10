e7510730000fa545d6bf85b90a376e9782830f19
Daksh Shah <dakshshah96@gmail.com>
2020-04-10 11:51:30 +0530

    stylelint: ignore tailwind at rule and use js file for config

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind'],
+      },
+    ],
+    indentation: 4,
+  },
+}
