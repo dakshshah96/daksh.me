fc6596167b6a4cab60c8cc6a89f9f37244503a81
Daksh Shah <dakshshah96@gmail.com>
2020-04-10 19:19:20 +0530

    basic footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,43 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer>
-      <div>I'm the footer</div>
+    <footer className="flex items-center justify-between h-32 max-w-4xl px-4 m-auto">
+      <div className="flex">
+        <a
+          href="https://www.buymeacoffee.com/dakshshah96"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-6 text-gray-600 hover:text-gray-900"
+        >
+          Buy me a coffee
+        </a>
+        <a
+          href="https://daksh.me/rss.xml"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-600 hover:text-gray-900"
+        >
+          RSS
+        </a>
+      </div>
+      <div className="flex">
+        <a
+          href="https://twitter.com/dakshshah96"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-6 text-gray-600 hover:text-gray-900"
+        >
+          Twitter
+        </a>
+        <a
+          href="https://github.com/dakshshah96"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-600 hover:text-gray-900"
+        >
+          GitHub
+        </a>
+      </div>
     </footer>
   )
 }
