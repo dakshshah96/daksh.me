b1b7404d9eab105d63a25eb4b6905a2107d9d9e8
Daksh Shah <dakshshah96@gmail.com>
2020-04-11 19:22:02 +0530

    stylelint: add @responsive to ignoreAtRules

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'at-rule-no-unknown': [
       true,
       {
-        ignoreAtRules: ['tailwind'],
+        ignoreAtRules: ['tailwind', 'responsive'],
       },
     ],
     indentation: 2,
