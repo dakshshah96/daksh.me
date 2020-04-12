c9a79e92c5c46aa695e826c4151dea9a5c9da581
Daksh Shah <dakshshah96@gmail.com>
2020-04-12 12:03:39 +0530

    init post template and delete default templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -108,8 +108,6 @@ exports.createPages = ({ graphql, actions }) => {
   return new Promise((resolve, reject) => {
     const postPage = path.resolve('src/templates/post.js')
     const pagePage = path.resolve('src/templates/page.js')
-    const tagPage = path.resolve('src/templates/tag.js')
-    const categoryPage = path.resolve('src/templates/category.js')
 
     resolve(
       graphql(
@@ -119,8 +117,6 @@ exports.createPages = ({ graphql, actions }) => {
               edges {
                 node {
                   frontmatter {
-                    tags
-                    categories
                     template
                   }
                   fields {
@@ -137,22 +133,7 @@ exports.createPages = ({ graphql, actions }) => {
           reject(result.errors)
         }
 
-        const tagSet = new Set()
-        const categorySet = new Set()
-
         result.data.allMarkdownRemark.edges.forEach((edge) => {
-          if (edge.node.frontmatter.tags) {
-            edge.node.frontmatter.tags.forEach((tag) => {
-              tagSet.add(tag)
-            })
-          }
-
-          if (edge.node.frontmatter.categories) {
-            edge.node.frontmatter.categories.forEach((category) => {
-              categorySet.add(category)
-            })
-          }
-
           if (edge.node.frontmatter.template === 'post') {
             createPage({
               path: edge.node.fields.slug,
@@ -172,28 +153,6 @@ exports.createPages = ({ graphql, actions }) => {
               },
             })
           }
-        })
-
-        const tagList = Array.from(tagSet)
-        tagList.forEach((tag) => {
-          createPage({
-            path: `/tags/${kebabCase(tag)}/`,
-            component: tagPage,
-            context: {
-              tag,
-            },
-          })
-        })
-
-        const categoryList = Array.from(categorySet)
-        categoryList.forEach((category) => {
-          createPage({
-            path: `/categories/${category.toLowerCase()}/`,
-            component: categoryPage,
-            context: {
-              category,
-            },
-          })
         })
       })
     )
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,45 +1,69 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 import { graphql } from 'gatsby'
 import Layout from '../layout'
 import UserInfo from '../components/UserInfo'
-import PostTags from '../components/PostTags'
 import SEO from '../components/SEO'
-import Footer from '../components/Footer'
 import config from '../../data/SiteConfig'
-import './b16-tomorrow-dark.css'
-import './post.css'
+import { formatDate, editOnGithub } from '../utils'
 
-export default class PostTemplate extends React.Component {
-  render() {
-    const { data, pageContext } = this.props
-    const { slug } = pageContext
-    const postNode = data.markdownRemark
-    const post = postNode.frontmatter
-    if (!post.id) {
-      post.id = slug
-    }
+export default function PostTemplate({ data, pageContext }) {
+  const { slug } = pageContext
+  const postNode = data.markdownRemark
+  const post = postNode.frontmatter
 
-    return (
-      <Layout>
-        <div>
-          <Helmet>
-            <title>{`${post.title} | ${config.siteTitle}`}</title>
-          </Helmet>
-          <SEO postPath={slug} postNode={postNode} postSEO />
-          <div>
-            <h1>{post.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
-            <div className="post-meta">
-              <PostTags tags={post.tags} />
-            </div>
-            <UserInfo config={config} />
-            <Footer config={config} />
-          </div>
-        </div>
-      </Layout>
-    )
+  if (!post.id) {
+    post.id = slug
   }
+
+  const date = formatDate(post.date)
+  const githubLink = editOnGithub(post)
+  const twitterShare = `http://twitter.com/share?text=${encodeURIComponent(
+    post.title
+  )}&url=${config.siteUrl}/${post.slug}/&via=dakshshah96`
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>{`${post.title} – ${config.siteTitle}`}</title>
+      </Helmet>
+      <SEO postPath={slug} postNode={postNode} postSEO />
+      <article className="max-w-4xl px-4 m-auto">
+        <header className="mb-12">
+          <div>
+            <h1 className="mb-4 text-3xl font-bold leading-tight">
+              {post.title}
+            </h1>
+            <div className="flex text-sm">
+              <time className="text-gray-600">{date}</time>
+              <span className="mx-4 text-gray-500">|</span>
+              <a
+                href={twitterShare}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-bold text-gray-600 hover:text-indigo-500"
+              >
+                Share on Twitter
+              </a>
+              <span className="mx-4 text-gray-500">|</span>
+              <a
+                href={githubLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-bold text-gray-600 hover:text-indigo-500"
+              >
+                Edit on GitHub
+              </a>
+            </div>
+          </div>
+        </header>
+
+        <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
+      </article>
+      <UserInfo config={config} />
+    </Layout>
+  )
 }
 
 /* eslint no-undef: "off" */
@@ -51,10 +75,9 @@ export const pageQuery = graphql`
       excerpt
       frontmatter {
         title
-        cover
+        slug
         date
-        category
-        tags
+        template
       }
       fields {
         slug
@@ -63,3 +86,27 @@ export const pageQuery = graphql`
     }
   }
 `
+
+PostTemplate.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      html: PropTypes.string,
+      timeToRead: PropTypes.number,
+      excerpt: PropTypes.string,
+      frontmatter: PropTypes.shape({
+        title: PropTypes.string,
+        slug: PropTypes.string,
+        date: PropTypes.string,
+        template: PropTypes.string,
+        id: PropTypes.string,
+      }).isRequired,
+      fields: PropTypes.shape({
+        slug: PropTypes.string,
+        date: PropTypes.string,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+  pageContext: PropTypes.shape({
+    slug: PropTypes.string,
+  }).isRequired,
+}
