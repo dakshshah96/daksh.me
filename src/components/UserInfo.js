a11778620081031ed0c45293821d2c2fc7117389
Daksh Shah <dakshshah96@gmail.com>
2020-04-17 18:11:15 +0530

    update styling for user info box component

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -3,24 +3,24 @@ import daksh from '../images/daksh.png'
 
 export default function UserInfo() {
   return (
-    <aside className="max-w-4xl px-4 py-10 m-auto mt-16 mb-10 bg-purple-900 rounded-lg">
+    <aside className="max-w-4xl px-4 py-10 m-auto mt-16 mb-10 rounded-lg user-info-box-gradient">
       <div className="flex px-4">
         <img
           src={daksh}
           alt="Daksh"
-          className="w-24 h-24 mr-8 border-4 border-purple-300 border-solid rounded-full"
+          className="w-24 h-24 mr-8 border-4 border-indigo-300 border-solid rounded-full"
         />
         <div>
-          <h3 className="mb-5 text-xl font-bold text-purple-100">
+          <h3 className="mb-5 text-xl font-bold text-indigo-100">
             Thanks for reading!
           </h3>
-          <p className="mb-8 text-purple-100">
+          <p className="mb-8 text-indigo-100">
             Hi, I’m Daksh, the author of this post and a full stack software
             engineer. If you enjoyed reading this post or anything else I’ve
             written, please consider supporting me!
           </p>
           <a
-            className="flex items-center justify-center w-48 py-2 text-purple-900 bg-purple-100 rounded-md hover:bg-purple-200"
+            className="flex items-center justify-center w-48 py-2 text-indigo-900 bg-gray-100 rounded-md hover:bg-gray-300"
             target="_blank"
             rel="noopener noreferrer"
             href="https://www.buymeacoffee.com/dakshshah96"
